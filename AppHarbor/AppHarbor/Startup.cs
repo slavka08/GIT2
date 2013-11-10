4893f977ed9da35da5b11e2516661feb4e411034
Slavik <slavintos@gmail.com>
2013-11-10 21:35:17 +0300

    initial commit

---
+++ AppHarbor/AppHarbor/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AppHarbor.Startup))]
+namespace AppHarbor
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
